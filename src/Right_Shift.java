6a4122ba1ed169b946b07f536e4a436ad13b9a6f
SRINIVAS BANDARU <bandarusrinivas626@gmail.com>
2025-08-07 01:12:55 +0530

    Update Right_Shift.java

--- src/Right_Shift.java
+++ src/Right_Shift.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Right_Shift {
+public class  Rotate_Right_1position{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int temp=arr[arr.length-1];
